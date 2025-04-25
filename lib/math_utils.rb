cf2b52a48e0b9490c9cfdcb585d074592db3f488
KristoMatsumoto <118841278+KristoMatsumoto@users.noreply.github.com>
2025-04-25 07:53:46 +0200

    Factorials method optimization

--- lib/math_utils.rb
+++ lib/math_utils.rb
@@ -1,9 +1,6 @@
 module MathUtils
   def self.fibonacci(n)
     raise ArgumentError, 'n must be a positive integer' unless n.is_a?(Integer) && n > 0
-    #     n.positive? 
-    # can be changed on        
-    #     n.is_a?(Integer) && n > 0
     
     fibs = [0, 1]
     (2...n).each do |i| fibs << fibs[-1] + fibs[-2] end
@@ -13,6 +10,8 @@ module MathUtils
   def self.factorials(n)
     raise ArgumentError, 'n must be a positive integer' unless n.is_a?(Integer) && n > 0
 
-    (0...n).map do |i| (1..i).reduce(1, :*) end
+    result = [1]
+    (1...n).each do |i| result << result[-1] * i end
+    result.first(n)
   end
 end
